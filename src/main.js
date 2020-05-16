4a85ade19768c4b08e92343f1466ebd9bce259d3
togoworld <59427625+togoworld@users.noreply.github.com>
2020-05-16 16:00:48 +0800

    装包

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from 'axios'
+import Vue from 'vue'  //依赖
+import App from './App.vue' //根组件
+// import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+// 
